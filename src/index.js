796e1982cdbb1e0c32f453cbce3fc1d5849c3a86
Katherine Milagros <katherinebemol17@gmail.com>
2022-05-31 12:08:49 -0500

    Segundo avance

--- src/index.js
+++ src/index.js
@@ -6,13 +6,38 @@ import validator from './validator.js';
  let primerBoton = document.getElementById("boton"); //traigo a mi botón de html a js
  primerBoton.addEventListener('click',()=> {   // llamo a mi boton que ahora es: primerBoton
  let creditCardNumber = document.getElementById("numerosIniciales").value; 
- console.log("Tú número de tarjeta es " +creditCardNumber );
+ //console.log("Tú número de tarjeta es " +creditCardNumber );
 
- // Llamar a mi js validator
+ // Llamar a mi js validator - función isValid
  validator.isValid(creditCardNumber);
+
+ console.log (creditCardNumber);
+ console.log(validator.isValid(creditCardNumber));
+
+    if (validator.isValid(creditCardNumber) == true){
+       document.querySelector('#respuesta').innerHTML ="Número de tarjeta valida";
+      
+    }
+    else {
+      document.querySelector('#respuesta').innerHTML = "Número de tarjeta no valida";
+      //alert ();
+    }
+   
+ // Llamar a mi js validator - función maskify
+
+ //console.log (validator.maskify(creditCardNumber));
+
+ let finalEmascarado = validator.maskify(creditCardNumber);
+ document.getElementById("numerosIniciales").value = finalEmascarado;
 
 
 });
+
+const cargarNuevamente = document.getElementById('volverInicio');
+cargarNuevamente.addEventListener ('click' , () => location.reload ());
+
+
+
 
 
 
@@ -28,6 +53,10 @@ import validator from './validator.js';
       //  alert ("La tarjeta no es válida");
     //}
   
+
+
+
+
  // Llamar al boton
  //let primerBoton = document.getElementById("boton"); //traigo a mi botón de html a js
  //primerBoton.addEventListener('click',()=> {   // llamo a mi boton que ahora es: primerBoton
--- src/validator.js
+++ src/validator.js
@@ -1,46 +1,87 @@
 const validator = {
   
  // Función validar
-  isValid:function name(creditCardNumber) {
+  isValid:function(creditCardNumber) {
     const arrayRevertido = [...creditCardNumber].reverse(); 
     console.log(arrayRevertido);
 
-    let arrayPares = arrayRevertido.filter(i => i % 2==0); // evaluar si es divisible entre 2
-    let arrayImpares = arrayRevertido.filter(i => i % 2!=0); // evaluar si no es igual a 0
-    console.log(arrayPares);
+    let arrayPares = arrayRevertido.filter((elemento,i)=> i % 2!=0); // 
+    let arrayImpares = arrayRevertido.filter((elemento,i) => i % 2==0); // 
+    //console.log(arrayPares);
     console.log(arrayImpares);
 
     let sumaNuevosPares = 0;
-    //let sumaNuevosImpares = 0;
-    //let devuelve;
+    let sumaNuevosImpares = 0;
+    let devuelve;
 
    // Para recorrer mi array pares y realizar las operaciones
    for (let i = 0 ; i < arrayPares.length ; i++ ) {  //cada que termina el ciclo el i vale 1+ - i=posición del ciclo
        let convertirString = parseInt (arrayPares[i]); //Convierte (parsea) un argumento de tipo cadena y devuelve un entero de la base especificada.
        let nuevosPares = convertirString*2;
-       console.log(nuevosPares);
+       //console.log(nuevosPares);
           if (nuevosPares >= 10) {
               nuevosPares = nuevosPares-9;
            }
          sumaNuevosPares = sumaNuevosPares + nuevosPares;
    }
-   console.log(sumaNuevosPares);
+   //console.log(sumaNuevosPares);
 
    // Para recorrer mi array impares y realizar la operación
+   for (let i = 0 ; i < arrayImpares.length ; i++ ){
+        sumaNuevosImpares = sumaNuevosImpares + parseInt (arrayImpares[i]);
+    }
+    //console.log(sumaNuevosImpares);
 
+   let totalSuma = sumaNuevosPares + sumaNuevosImpares;
+     if (totalSuma % 10 == 0){
+       devuelve = true;
+       
+      }
+     else {
+       devuelve = false;
+     }
 
+    return devuelve;
+    //console.log(devuelve);
+    
+  } ,
+ 
+  //Función maskify
+ 
 
+  maskify:function(creditCardNumber){
+  //console.log (creditCardNumber);  
+  let totalEnmascarado;
+  let enmascarar = [];
 
+  let separar = creditCardNumber.split ("");
+  separar.forEach((elemento , i) => {
+   if (i < separar.length - 4) {
+     elemento = "#";
+     //console.log(elemento);
+   }
+   
+   enmascarar.push(elemento);
+   //console.log(enmascarar);
+   totalEnmascarado = enmascarar.join ('');
 
+ });
+ //console.log (totalEnmascarado);
+ return totalEnmascarado;
 
-  } 
-
-
+ 
+  }
 
 }
-
-
 
 export default validator;
 
 
+//return creditCardNumber.split (' ').map ((numeros,i) => 
+//i < creditCardNumber.length - 4 ? '#' : numeros). join(' ');
+
+
+
+//var miCadena = "Hola Mundo. Cómo estás hoy?";
+//var divisiones = miCadena.split(" ", 3);
+//console.log(divisiones);
